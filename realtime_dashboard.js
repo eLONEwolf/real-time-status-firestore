a7e37e5c641c2fb11f19e43a40e90433f1803166
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2023-10-18 08:00:50 +0000

    adding frontend socket emit changes

--- realtime_dashboard.js
+++ realtime_dashboard.js
@@ -1,1 +1,18 @@
 // make a dashboard to show stuff here
+
+firebase.firestore().collection('status')
+    .where('state', '==', 'online')
+    .onSnapshot(function(snapshot) {
+        snapshot.docChanges().forEach(function(change) {
+            if (change.type === 'added') {
+                var msg = 'User ' + change.doc.id + ' is online.';
+                console.log(msg);
+                // ...
+            }
+            if (change.type === 'removed') {
+                var msg = 'User ' + change.doc.id + ' is offline.';
+                console.log(msg);
+                // ...
+            }
+        });
+    });
